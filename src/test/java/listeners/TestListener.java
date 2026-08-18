0999cbf7202e1c9a5afea1aacd5f331cd151f520
alexlefy <alex.puscuta@yahoo.com>
2026-08-18 10:46:38 +0300

    feat: add TestListener to capture screenshots on test failure

---
+++ src/test/java/listeners/TestListener.java
@@ -0,0 +1,19 @@
+package listeners;
+
+import com.helios.driver.DriverManager;
+import com.helios.utils.ScreenshotUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+        WebDriver driver = DriverManager.get();
+
+        ScreenshotUtils.capture(
+                driver,
+                result.getName()
+        );
+    }
+}
